ebdce7751bc77c946bf364d2232ca0918107eafa
Belleza231 <afuaahemah@icloud.com>
2026-07-27 21:18:54 +0000

    add assignment_05_fibonacci_sequence.cpp

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,78 @@
 #include <iostream>
 using namespace std;
 
+// Function prototypes
+void printFibonacciTerms(int n);
+bool isFibonacciNumber(int num);
+
+int main() {
+    int choice;
+
+    cout << "--- FIBONACCI SEQUENCE GENERATOR ---" << endl;
+    cout << "1. Print First N Terms" << endl;
+    cout << "2. Check if Number is Fibonacci" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        int n;
+        cout << "\nHow many terms? ";
+        cin >> n;
+
+        if (n <= 0) {
+            cout << "Error: N must be a positive integer!" << endl;
+        } else {
+            cout << "Fibonacci sequence: ";
+            printFibonacciTerms(n);
+            cout << endl;
+        }
+    } 
+    else if (choice == 2) {
+        int num;
+        cout << "\nEnter a number to check: ";
+        cin >> num;
+
+        if (isFibonacciNumber(num)) {
+            cout << num << " is a Fibonacci number." << endl;
+        } else {
+            cout << num << " is NOT a Fibonacci number." << endl;
+        }
+    } 
+    else {
+        cout << "Invalid choice!" << endl;
+    }
+    return 0;
+}
+
+// Function to print first N Fibonacci terms
+void printFibonacciTerms(int n) {
+    int first = 0;
+    int second = 1;
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            cout << first;
+        } else {
+            cout << " " << second;  
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+
+// Function to check if a number is Fibonacci
+bool isFibonacciNumber(int num) {
+    if (num < 0) return false;
+    if (num == 0 || num == 1) return true;
+    
+    int first = 0;
+    int second = 1;
+    int next = 1;
+    
+    while (next < num) {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return next == num;
+}
